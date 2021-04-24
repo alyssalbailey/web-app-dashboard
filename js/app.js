d457da3c29c32670b0c77e90fe525feb23ccf4fb
alyssalbailey <alyssabailey@me.com>
2021-04-24 13:00:13 -0400

    New mockups added to the project - fixing layout to align with new style guide.

---
+++ js/app.js
@@ -0,0 +1,60 @@
+
+const alert =  document.getElementById('alert');
+
+// create the html for the banner
+alertBanner.innerHTML = 
+`
+<div class="alert-banner">
+    <p><strong>Alert:</strong> You have <strong>6</strong> over tasks to complete</p>
+    <p class="alert-banenr-close">x</p>
+</div>
+`
+
+alertBanner.addEventListener ('click', e => {
+    const element = e.target;
+    if (element.classList.contains("alert-banner-close")) {
+        alert.style.display = "none"
+    }
+});
+
+
+// Traffic Chart
+const trafficCanvas = document.getElementById('traffic-chart');
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+        backgroundColor: 'rgba(116, 119, 191,.3)',
+        borderWidth: 1;
+    }]
+};
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    animation: {
+        duration: 0
+    },
+    scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+        display: false
+        }
+    }
+};
+
+//Creating the actual chart
+function createTrafficChart(data) {
+    let trafficChart = new Chart(trafficCanvas, {
+        type: 'line',
+        data: trafficData,
+        options: trafficOptions
+    });
+}
+
+
+
